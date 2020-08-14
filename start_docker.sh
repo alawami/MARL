d2235d6e31f01608f5b211301923801b444fd711
alawami <ahmed.alawami+github@alumni.colostate.edu>
2020-08-14 15:10:36 -0600

    improve docker run

--- start_docker.sh
+++ start_docker.sh
@@ -1,1 +1,1 @@
-docker run --gpus all --name tennis --rm -it -v "$(pwd)":/rl -p 8900:8888 aalawami/banana_rl:cuda10.2 --
+docker run --gpus all --name tennis --rm -it -v "$(pwd)":/rl -p 8900:8888 --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 aalawami/banana_rl:cuda10.2 --
